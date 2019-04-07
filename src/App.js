007befa2ab586f3b18234e3a7cb8622ef145bd7d
unknown <lugowski.k@gmail.com>
2019-04-07 21:01:07 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const data = [
+  {
+    id: 1, title: 'Wiadomość numer 1', body: 'Labore non pariatur sint elit ad eiusmod cupidatat enim ad duis non veniam. Treść wiadomości numer '
+  }
+]
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `${data[0].body} ${index}`
+  })
+}, 4000);
+
+
 class App extends Component {
+  state = {
+    comments: [...data]
+  }
+
+  getData() {
+    if (this.state.comments.length !== data.length) {
+      this.setState({ comments: [...data] })
+    }
+  }
+
+  componentDidMount() {
+    this.intervalIndex = setInterval(() => {
+      this.getData()
+    }, 2000);
+  }
+
+
+  componentWillUnmount() {
+    clearInterval(this.intervalIndex)
+  }
+
   render() {
+
+    const section = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        < h1 > {comment.title}</h1 >
+        <p>{comment.body}</p>
+      </div>
+
+    ))
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {section.reverse()}
       </div>
     );
   }
